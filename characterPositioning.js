8f9a36c27a1ed5a0da8ae71942d524cb302888e1
Taylor-Cameron <taylorcameron95@hotmail.ca>
2018-10-24 20:08:43 +0000

    characterPositioning assignment

---
+++ characterPositioning.js
@@ -0,0 +1,29 @@
+
+function letterInd(string) {
+  var finalObject = {};
+
+  var lower = string.toLowerCase();
+  for(var i = 0; i < lower.length; i++) {
+    letter = lower[i];
+    if(!finalObject[letter]) {
+      finalObject[letter] = [i];
+    } else {
+      finalObject[letter].push(i);
+    }
+
+}
+  console.log(finalObject);
+}
+letterInd('lighthouse in the house');
+// {
+//   l: 0
+//   i: 1, 11
+//   g: 2
+//   h: 3, 5, 15, 18
+//   t: 4, 14
+//   o: 6, 19
+//   u: 7, 20
+//   s: 8, 21
+//   e: 9, 16, 22
+//   n: 12
+// }
